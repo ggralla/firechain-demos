4b8afeb65e7f12e32692f46a971551db9d9750aa
Gabe Gralla <gabe.gralla@gmail.com>
2020-12-22 20:58:08 +0000

    utf8 and namespaces

--- helpers/firechain.js
+++ helpers/firechain.js
@@ -14,21 +14,27 @@ if (!ethPrivKey) {
 // TODO what is wallet vs account?
 const account = w3.eth.accounts.privateKeyToAccount(ethPrivKey);
 
+
+// TODO: public vs private
+const NAMESPACE = "0xfBEd705f2BC14897A008425189aAA66d2Ae387c1";
+//const NAMESPACE = account.address;
+console.log(NAMESPACE)
+
 const CONTRACT_ADDR = w3.utils.toChecksumAddress("0x02dB0556424733e0a4a85970c9A104E20555415E");
 export const contract = new w3.eth.Contract(abi, CONTRACT_ADDR);
 console.log(account);
 contract.options.from = account.address;
 
 export function fireRead(key) {
-    const convKey = w3.utils.asciiToHex(key).padEnd(66,"0") ;
+    const convKey = w3.utils.utf8ToHex(key).padEnd(66,"0") ;
     const ret = contract.methods
         .read(account.address, convKey)
         .estimateGas()
         .then((gasEstimate) => {
           contract.methods
-            .read(account.address, convKey)
+            .read(NAMESPACE, convKey)
             .call().then((response) => {
-                console.log("read", response);
+                console.log("read", w3.utils.toUtf8(response[0]));
             })
     })
     .catch(error=> console.log("READ ERROR", error))
@@ -36,16 +42,16 @@ export function fireRead(key) {
 }
 
 export async function fireWrite(key, value) {
-    const convKey = w3.utils.asciiToHex(key).padEnd(66,"0") ;
-    const convValue = w3.utils.asciiToHex(value).padEnd(66,"0") ;
+    const convKey = w3.utils.utf8ToHex(key).padEnd(66,"0") ;
+    const convValue = w3.utils.utf8ToHex(value).padEnd(66,"0") ;
     //const nonce = await w3.eth.getTransactionCount(account.address, 'pending')
-    const ret = await contract.methods.write(account.address, convKey, {value: convValue}).send({gas:99999});
-    console.log(ret)
+    const ret = await contract.methods.write(NAMESPACE, convKey, {value: convValue}).send({gas:99999});
+    console.log("write", ret)
     return ret;
 }
 
 export async function fireCreate() {
-    const ret = await contract.methods.createStore(account.address).send({gas:999999});
-    console.log(ret)
+    const ret = await contract.methods.createStore(NAMESPACE).send({gas:999999});
+    console.log("create", ret)
     return ret;
 }
